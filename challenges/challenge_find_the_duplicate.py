6d7340b083e1d697e1f8ad1d5fc524dd6201c54d
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-18 03:38:44 -0300

    requisito 05 - feita a função "find_duplicate" porém ainda não passou no teste de complexidade pq ta demorando na maquina local

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,53 @@
+def sort_num_list(num_list, start=0, end=None):
+    if end is None:
+        end = len(num_list)
+
+    if (end - start) == 1:
+        if (
+            num_list is None
+            or type(num_list[start:end][0]) != int
+            or num_list[start:end][0] < 1
+        ):
+            raise ValueError
+
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        sort_num_list(num_list, start, mid)
+        sort_num_list(num_list, mid, end)
+        merge_sort(num_list, start, mid, end)
+
+
+def merge_sort(num_list, start, mid, end):
+    left = num_list[start:mid]
+    right = num_list[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            num_list[general_index] = right[right_index]
+            right_index = right_index + 1
+
+        elif right_index >= len(right):
+            num_list[general_index] = left[left_index]
+            left_index = left_index + 1
+
+        elif left[left_index] < right[right_index]:
+            num_list[general_index] = left[left_index]
+            left_index = left_index + 1
+
+        else:
+            num_list[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        sort_num_list(nums)
+    except ValueError:
+        return False
+
+    for index in range(1, len(nums)):
+        if nums[index] == nums[index - 1]:
+            return nums[index]
+    return False

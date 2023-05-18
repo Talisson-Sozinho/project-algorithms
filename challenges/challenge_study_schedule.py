82bee84e168cf3d4042ef440104da7051ca7c8b7
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-17 22:29:44 -0300

    requisito 01 - feita a função "study_schedule"

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not permanence_period or not target_time:
+        return None
+
+    count = 0
+
+    for check_in, check_out in permanence_period:
+        if (
+            type(check_out) != int
+            or type(check_in) != int
+            or check_in > check_out
+            or check_in < 0
+            or check_out < 0
+        ):
+            return None
+
+        if check_in <= target_time <= check_out:
+            count += 1
+
+    return count

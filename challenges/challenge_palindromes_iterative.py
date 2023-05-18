5deefec6dd41872fcdebb3088fd3bf3cb8d50e5d
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-18 03:37:41 -0300

    requisito 06 - feito a função "is_palindrome_iterative"

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    for index in range(len(word)):
+        if word[index] != word[-index - 1]:
+            return False
+
+    return True
